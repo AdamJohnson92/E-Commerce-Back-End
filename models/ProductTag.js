85257fd40571d4065c6fc4e23d6e75b4bf2194d8
Adam <adamgjohnson92@gmail.com>
2023-07-17 11:47:04 -0700

    Added foreign key references to Product and Product Tags models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,13 +6,25 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    product_id: {
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
     },
     tag_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'id'
+      },
     }
   },
   {
